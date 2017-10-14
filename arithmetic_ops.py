0e27f15ae3618316e3205a6811d82954f39353ac
Arthur Ngondo <ngondoarthur@gmail.com>
2017-10-14 13:55:27 +0300

    Flesh out floor division routine in AritmeticOperations class.

--- arithmetic_ops.py
+++ arithmetic_ops.py
@@ -42,7 +42,7 @@ class ArithmeticOperations(object):
                 calc.exponentiation_func()
 
             elif arithmetic_selection == '4': # Floor Division
-                pass
+                calc.floor_division_func()
 
             elif arithmetic_selection == '5': # Modulo
                 pass
@@ -107,7 +107,15 @@ class ArithmeticOperations(object):
         
         Prints num1 // num2 = result (exactly two decimal places.)
         '''
-        pass
+        print(format(' Floor Division  ', '.^78' ))
+        print(format(' first value // second value  ', '.^78' ))
+        num1 = float(input('Enter the first  value: '))
+        num2 = float(input('Enter the second  value: '))
+        print('\n')
+        print('{0} // {1} = {2:.2f} or {2:.2e}'.format(num1, num2, num1 // num2))
+        print('\n')
+        return_to_menu = input('\tPress A key to Return to Main menu ->')
+
     def modulo_func(self):
         '''performs modulo division on two numbers from user.  
         Takes integers, floating-point and scientific notation numbers. 
