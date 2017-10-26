69b401a0fe5a216a77f691214e0513c72fb3b7af
Arthur Ngondo <ngondoarthur@gmail.com>
2017-10-26 20:29:38 +0300

    Add calender month readme & problem analysis

---
+++ calender_month.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+''' A Calender Month Program
+
+Displays a calendar month for any given month between January 1800 and December
+2099. 
+'''
+# Problem Analysis
+##################
+# TODO: compute the first day of a given month for years 
+# 1800-2099
+
+# TODO: Display the calender month, given the day of the week that the first
+# day falls on, and the number of days in the month
+
