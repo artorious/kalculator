b07f0768a2cdfff023ed9c5e68dd4d7782535474
Arthur Ngondo <ngondoarthur@gmail.com>
2017-11-06 22:20:46 +0300

    Construct calender year data structure

--- scripts/calender_year.py
+++ scripts/calender_year.py
@@ -11,7 +11,12 @@ Display Features:
 
 # Init
 calendar_year = []     # calendar_year = [ [ calendar_month ], [ calendar_month ], etc.] ]
-calendar_month = []    # calendar_month = [ week_1, week_2, . . ., week_k ]
+days_in_month = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31) # TODO: handle Feb for leap
+month_names = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                'August', 'September', 'October', 'November', 'December')
+month_separator = format(' ', '8')
+blank_week = format(' ', '21')
+blank_col = format(' ', '3')
 
 terminate = False   # Control program termination
 
@@ -19,7 +24,8 @@ terminate = False   # Control program termination
 print('=' * 120)
 print(format(' CALENDER YEAR ', '=^120'))
 print('=' * 120)
-print(format('Displays a Calendar for any given year between 1800 and 2099', '^120'))
+print(format('Displays a Calendar for any given year between 1800 and 2099', 
+'^120'))
 print('-' * 120)
 
 while not terminate:    # Prompt for yrs until quit
@@ -28,7 +34,8 @@ while not terminate:    # Prompt for yrs until quit
 
         while (year not in range(1800, 2100) and year != -1): # Check year is within appropriate range
             try:
-                print(format(' Enter Year (1800-2099) OR Enter -1 to Quit. ', '~^120'))
+                print(format(' Enter Year (1800-2099) OR Enter -1 to Quit. ',
+                 '~^120'))
                 year = int(input('Enter year [YYYY] : '))
             
             except ValueError:
@@ -57,20 +64,69 @@ while not terminate:    # Prompt for yrs until quit
             if not leap_year:   # Leap yr check
                 value += 1
 
-            first_day_of_month = (value + 1) % 7    #  first day day of month for Jan 1
+            #  first day day of month for Jan 1. the days of the week for all remaining dates simply follow.
+            first_day_of_current_month = (value + 1) % 7    
+            
+            # Construct calender for all 12 months
+            for month_num in range(12):
+                month_name = month_names[month_num]
 
-            print('Day of the week is : ', first_day_of_month)
+                # Init for new month
+                current_day = 1
+                if first_day_of_current_month == 0:     # Saturday
+                    starting_col = 7 
+                else:
+                    starting_col = first_day_of_current_month
+                # Re-assign
+                current_col = 1
+                calendar_month = []    # calendar_month = [ week_1, week_2, . . ., week_k ]
+                calendar_week = ''
+
+                # Add any needed leading space for the first week of month
+                while current_col < starting_col:
+                    calendar_week += blank_col
+                    current_col += 1
+                
+                # Store month as separate weeks
+                if (month_name == 'February') and leap_year:
+                    num_days_this_month = 29
+                else:
+                    num_days_this_month = days_in_month[month_num]
+                
+                while current_day <= num_days_this_month:
+                    # Store day of month in field of length 3
+                    calendar_week += format(str(current_day), '>3')     # append each date with auto-alignment
+
+                    # Check if at the last column of the week after each append
+                    if current_col == 7:
+                        calendar_month += [calendar_week] # Append a week string
+                        calendar_week = '' # Re-init
+                        current_col = 1 # Reset
+                    else:
+                        current_col += 1
+                    
+                    # increament current day
+                    current_day += 1
+                
+                # fill out final row of month with needed blanks
+                calendar_week += blank_week[0:(7-current_col+1) * 3]
+                calendar_month += [calendar_week]
+
+                # reset values for next month
+                first_day_of_current_month = current_col
+                calendar_year += [calendar_month]
+                calendar_month = []
+
+        print(calendar_year)    
+
+        # reset for another year
+        calendar_year = []
+
 
     except ValueError:
         print('INVALID input......Enter year [YYYY]')
 
 
-
-
-
-
-    # No. of days in each month (accounting for leap yrs)
-    # Names of each of the 12 months for display
 # Construct Calender year
 
 # Display Calender year
