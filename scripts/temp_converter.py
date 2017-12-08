9311a71528b1cc51404a6e65ecd1e75686baeafe
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-08 13:15:02 +0300

    flesh-out convert_temp()

--- scripts/temp_converter.py
+++ scripts/temp_converter.py
@@ -14,7 +14,7 @@ def get_conversion_type():
     '''Prompt user for Conversion Type.
     Fahreheit to Celcius & vice-versa
     '''
-    pass
+    return
 
 def display_fahren_to_celcius(start, end):
     '''convert a range of values from Fahrenheit to Celsius.'''
@@ -29,7 +29,24 @@ def convert_temp():
     '''convert a range of values from Fahrenheit to Celsius, 
     or Celsius to Fahrenheit.
     '''
-    pass
+    display_welcome() # Program Greeting
+    which = get_conversion_type() # Assign Conversion Type
+    
+    # Prompt User for values
+    proper_conversion_units = False
+
+    while not proper_conversion_units: # Error-Check user input
+        try:
+            temp_start = int(input('Enter Starting temperature value to convert: '))
+            temp_end = int(input('Enter Ending temperature value to convert: '))
+            proper_conversion_units = True
+        except ValueError:
+            print('Expected integers')
+
+    if which == 'F':
+        display_fahren_to_celcius(temp_start, temp_end)
+    else:
+        display_celcius_to_fahren(temp_start, temp_end)
 
 if __name__ == '__main__':
     convert_temp()
