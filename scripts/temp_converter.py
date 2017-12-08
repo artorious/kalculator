375126a1a11ef25d20b415729c25e92b95f19138
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-08 12:47:24 +0300

    Add temparature conversion scripts with stubbed functions.

---
+++ scripts/temp_converter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Temperature Conversion Program
+The program allows a user to convert a range of values from Fahrenheit to Celsius, 
+or Celsius to Fahrenheit.
+The script utilizes the following programming features.
+    ➤ value-returning functions
+    ➤ non-value-returning functions
+'''
+def display_welcome():
+    '''Diaplay Program greeting'''
+    pass
+
+def get_conversion_type():
+    '''Prompt user for Conversion Type.
+    Fahreheit to Celcius & vice-versa
+    '''
+    pass
+
+def display_fahren_to_celcius(start, end):
+    '''convert a range of values from Fahrenheit to Celsius.'''
+    pass
+
+def display_celcius_to_fahren(start, end):
+    '''convert a range of values from Celsius to Fahrenheit.'''
+    pass
+
+
+def convert_temp():
+    '''convert a range of values from Fahrenheit to Celsius, 
+    or Celsius to Fahrenheit.
+    '''
+    pass
+
+if __name__ == '__main__':
+    convert_temp()
